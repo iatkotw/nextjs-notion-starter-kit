6093444b32523821c993d5aeacf08bf8ed9fb1f1
iatkotw <111580420+iatkotw@users.noreply.github.com>
2023-10-17 02:24:00 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '62961c9bfc7d44729c100e8d09aaf458?pvs=4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,8 +10,8 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'https://hot-november-782.notion.site/Next-js-Notion-Starter-Kit-Template-62961c9bfc7d44729c100e8d09aaf458?pvs=4',
+  author: 'Yonghai Gong',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
